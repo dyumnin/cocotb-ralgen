069d3a0c5f566b021034f313d89164942d84b781
Vijayvithal Jahagirdar <jvs@dyumnin.com>
2024-10-21 14:14:28 +0530

    Pytest is passing.

--- tests/test_dma.py
+++ tests/test_dma.py
@@ -1,18 +1,7 @@
-import cocotb
-from cocotb.triggers import RisingEdge
-from DMA_Reg_RAL import DMA_Reg_RAL_Test as RAL
-from env import Env
+import pytest
+
+import subprocess
 
 
-@cocotb.test
-async def test_ral(dut):
-    env = Env(dut)
-    env.start()
-    ral = RAL(env.reg)
-    await run_ral_rw_check(env, ral)
-
-
-async def run_ral_rw_check(env, ral):
-    await env.reset_done()
-    await RisingEdge(env.dut.CLK)
-    await ral.rw_test()
+def test_dma():
+    subprocess.run("peakrdl cocotb_ralgen DMA.rdl -o .".split())
