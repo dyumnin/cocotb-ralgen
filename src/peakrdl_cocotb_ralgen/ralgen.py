30a7d040f6a566ac1d9194a8e12dc5c5e8784cf1
Vijayvithal Jahagirdar <jvs@dyumnin.com>
2024-10-21 12:37:54 +0530

    Moved print content to template

--- src/peakrdl_cocotb_ralgen/ralgen.py
+++ src/peakrdl_cocotb_ralgen/ralgen.py
@@ -1,4 +1,5 @@
 import sys
+from jinja2 import Environment, PackageLoader
 from pprint import PrettyPrinter
 from systemrdl import RDLCompiler
 from systemrdl import RDLListener
@@ -6,7 +7,7 @@ from systemrdl import RDLListener
 
 """
 # RAL Test
-RAL Test consists of 
+RAL Test consists of
 1. Reset read test
 2. Random Read Write tests
 
@@ -22,7 +23,7 @@ e.g.
 
 A check function can only check the modified register, or check all registers to ensure that only the desired bit in the desired register is modified.
 
-For every register we need to create 
+For every register we need to create
 1. A Reset value and Reset mask
 2. Write mask
 3. Read mask
@@ -75,7 +76,8 @@ logging.basicConfig(format=FORMAT)
             {
                 "reg": self.current_register,
                 "sig": node.get_path_segment(),
-                "shift": node.low,
+                "low": node.low,
+                "high": node.high,
             }
         )
         if "reset" in node.inst.properties:
@@ -125,64 +127,13 @@ logging.basicConfig(format=FORMAT)
             self.registers[self.current_register]["disable"].append("rw")
         if not node.has_sw_readable:
             self.registers[self.current_register]["disable"].extend(["rw", "reset"])
-        # print(f"{self.current_register}: {self.registers[self.current_register]['reset_value']:x}")
-        # print(f"{self.current_register} Mask: {self.registers[self.current_register]['reset_mask']:x}")
-        # print(f"{self.current_register} wr Mask: {self.registers[self.current_register]['write_mask']:x}")
-        # print(f"{self.current_register} rd Mask: {self.registers[self.current_register]['read_mask']:x}")
         pass
 
     def exit_Addrmap(self, node):
         preg = HexPP().pformat(self.registers)
-        print(
-            f"""
-class {node.get_path_segment()}_RAL_Test:
-    masks={preg}
-    def __init__(self,regmodel,bg_sigfn=None):
-        self.regmodel=regmodel
-        self.get_signal_value=bg_sigfn
-        pass
-    def reset_test(self,verbose=False):
-        for key,val in self.masks.items():
-            r=self.regmodel.__getattribute__(key)
-            if 'reset' in val['disable']:
-                continue
-            rv=r.read()
-            assert rv & val['reset_mask'] == val['reset_value'], "%s Resetvalue mismatch Actual %X,Expected %X," %(key,rv & val['reset_mask'], val['reset_value'])
-            if verbose:
-                logger.info("Reset Read Reg:%s, Value %s"%(key,rv))
-
-    async def rw_test(self,foreground_write=True,foreground_read=True,count=10,default_value=None,verbose=False):
-        # TODO Handle background oprations
-        assert foreground_write and foreground_read, "Error Background operations are not yet defined"
-        for key,val in self.masks.items():
-            if 'rw' in val['disable']:
-                continue
-            r=self.regmodel.__getattribute__(key)
-            rv=None
-            donttest=val['donttest']
-            for _ in range(count):
-                if default_value:
-                    wrval=default_value
-                else:
-                    wrval=random.randint(0,2**val['regwidth'])
-                wval=wrval & ~val['donttest']
-                await r.write(wval)
-                if foreground_read:
-                    rv=await r.read()
-                else:
-                    rv=0
-                    for hsh in val['signals']:
-                       rv|=self.get_signal_value(hsh)
-                wmask=val['write_mask']
-                rmask=val['read_mask']
-                actual=rv&wmask&~donttest
-                expected= wval& wmask &rmask
-                assert actual==expected,"%s:: Read Write Written %x, Read %x, Expected %x, wrMask %x rdmask %x, donttest = %x"%(key,wrval,actual,expected,wmask,rmask, donttest)
-            if verbose:
-                logger.info("Read Write Test Reg:%s, Value %s"%(key,rv))
-            """,
-            file=self.file,
-        )
+        env = Environment(loader=PackageLoader("peakrdl_cocotb_ralgen"))
+        template = env.get_template("ralgen.j2")
+        print(template.render(preg=preg, node=node), file=self.file)
 
 
 if __name__ == "__main__":
