d24788e2267b696440bec756bea6628f4ea096ec
Vijayvithal Jahagirdar <jvs@dyumnin.com>
2024-10-22 15:16:37 +0530

    Added project URL's removed fallback version.
    Fetch tags.
    
    debug
    
    Trying solution provided by PDM dev.
    
    testing
    
    testing

--- version.py
+++ version.py
@@ -4,8 +4,6 @@ from pdm.backend.hooks.version import SCMVersion
 
 def format_version(version: SCMVersion) -> str:
     """Used by pdm."""
-    with open("f", "w") as f:
-        print(version, file=f)
     if version.distance is None:
         return str(version.version)
     return f"{version.version}.post{version.distance}"
